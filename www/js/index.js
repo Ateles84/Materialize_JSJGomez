137bdef62b4aae1c761c86ca7296dbc4cf824858
Joan <juanbass84@gmail.com>
2020-02-13 20:18:34 +0100

    icona menu feta pero no funcional

--- www/js/index.js
+++ www/js/index.js
@@ -41,6 +41,20 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+
+
 };
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
 
 app.initialize();
